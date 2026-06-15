f41d5c08350db771fbe13f6c325d96757cfcdbc9
Angelica <olga.barreros@ucsp.edu.pe>
2026-06-15 17:39:33 -0500

    feat:agregar autenticacion basica en fronted Login.js

--- src/context/Actions.js
+++ src/context/Actions.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 export const Api = axios.create({
-    baseURL: 'http://localhost:8080'
+    baseURL: 'http://localhost:8080',
+    auth: {
+        username: '',
+        password: ''
+    }
 })
 
 export const LOGIN = 'LOGIN';
